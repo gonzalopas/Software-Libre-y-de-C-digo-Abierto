4abebbccff2150189a6c968c688c1bd8e835106e
Gonzalo Pastor <>
2024-02-18 17:49:32 +0100

    comit inicial

---
+++ sites/default/settings.php
@@ -0,0 +1,15 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'P4ssUi1!',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'LdsuUOn-nAESBaaanlKeQ3bMaSlKZ1rmVSv4Ptnf3JkCn8Bt_sPxGyyU28wbGV3p_U-u7-s7pg';
+$settings['config_sync_directory'] = 'sites/default/files/config_4dG7b_1LAMtZGSvTaIkWSVNuLWhlNk8XT5ymV_WTq0fjKWELICuGQ6Ok0ATwbUNpz0MVJudoeA/sync';
